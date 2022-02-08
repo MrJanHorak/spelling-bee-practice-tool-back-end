8a82392e221e0376b5861c57c7c31cd147ca67e4
Jan Horak <janny_b_goode@hotmail.com>
2022-02-07 21:57:43 -0500

    add word create control function

--- controllers/words.js
+++ controllers/words.js
@@ -1,30 +1,39 @@
-import { Word } from '../models/word.js'
+import { Word } from "../models/word.js";
 
 function index(req, res) {
   // Find all words
-  Word.find({}).sort({ word: "asc"})
-  .then((words) => {
-    res.render("words/index", {
-      words
+  Word.find({})
+    .sort({ word: "asc" })
+    .then((words) => {
+      res.render("words/index", {
+        words,
+      });
     })
-  })
-  .catch((err) => {
-    console.log(err)
-    res.redirect("/words")
-  })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/words");
+    });
 }
 
 function newWord(req, res) {
-  res.render("words/new")
+  res.render("words/new").catch((err) => {
+    console.log(err);
+    res.redirect("/words");
+  });
+}
+
+function create(req, res) {
+  req.body.visible = !!req.body.visible
+  Word.create(req.body)
+  .then((word) => {
+    res.redirect("/words")
+  })
   .catch((err) => {
     console.log(err)
-    res.redirect("/recipes")
+    res.redirect("/words)")
   })
-}
+  }
 
 
-export {
-  index,
-  newWord as new,
-  
-}
+
+export { index, newWord as new, create, };
