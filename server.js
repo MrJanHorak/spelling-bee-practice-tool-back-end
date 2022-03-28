2a9120c571a17bc64f8150dce7daaced574fc854
Jan Horak <janny_b_goode@hotmail.com>
2022-03-28 14:24:02 -0400

    refactor server.js

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 import "dotenv/config.js";
 import express from "express";
-// import path from "path";
-import { fileURLToPath } from "url";
-// import session from "express-session";
-
-// import methodOverride from "method-override";
 import logger from "morgan";
 import cors from "cors";
-// import { passUserToView } from "./middleware/auth.js";
 
 // connect to MongoDB with mongoose
 import('./config/database.js')
@@ -21,46 +15,18 @@ import { router as wordRouter } from "./routes/words.js"
 // create the express app
 const app = express();
 
-// view engine setup
-// app.set(
-//   "views",
-//   path.join(path.dirname(fileURLToPath(import.meta.url)), "views")
-// );
-// app.set("view engine", "ejs");
 
-//middleware
-// app.use(methodOverride("_method"));
 app.use(cors(
-  {
-    origin: 'https://spelling-bee-practice-app.netlify.app'
-  }
+  // {
+  //   origin: 'https://spelling-bee-practice-app.netlify.app'
+  // }
 ));
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   express.static(
-//     path.join(path.dirname(fileURLToPath(import.meta.url)), "public")
-//   )
-// );
 
-// // session middleware
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//       sameSite: "lax",
-//     },
-//   })
-// );
-
-// //custom middleware
-// app.use(passUserToView);
 
 // router middleware
-app.use("/", indexRouter)
+// app.use("/", indexRouter)
 app.use("/api/auth", authRouter);
 app.use('/api/profiles', profilesRouter)
 app.use("/api/words", wordRouter)
