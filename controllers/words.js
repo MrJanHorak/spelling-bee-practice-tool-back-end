ab8285f5f1efa767eff42e93a612577ba686d955
Jan Horak <janny_b_goode@hotmail.com>
2022-04-06 12:29:00 -0400

    change  sort of  word index controller

--- controllers/words.js
+++ controllers/words.js
@@ -1,7 +1,7 @@
 import { Word } from "../models/word.js";
 
 function index(req, res) {
-  Word.find({}).sort({word : asc})
+  Word.find({}).sort({'word' : asc})
     .then((words) => {
       res.status(201).json(words);
     })
