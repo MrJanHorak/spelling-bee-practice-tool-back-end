28c4defe5dfe084862139c0eead7b2a33fb697fe
Jan Horak <janny_b_goode@hotmail.com>
2022-04-29 22:21:20 -0400

    bugFix/updatePracticedWord route typo

--- routes/profiles.js
+++ routes/profiles.js
@@ -12,6 +12,6 @@ router.get("/", checkAuth, profilesCtrl.index);
 router.get("/:id", checkAuth, profilesCtrl.show);
 router.put("/:id", checkAuth, profilesCtrl.update);
 router.post("/:id/practicedWords", checkAuth, profilesCtrl.addPracticedWord);
-router.put("/:id/practicedWords/:practicedWordId", checkAuth, groupCtrl.updatePracticedWord);
+router.put("/:id/practicedWords/:practicedWordId", checkAuth, profilesCtrl.updatePracticedWord);
 
 export { router };
