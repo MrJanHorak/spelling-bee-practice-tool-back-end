ed6fc4b61e41e2165d233f375b5b2d7ad1fd4d34
Jan Horak <janny_b_goode@hotmail.com>
2022-05-24 12:13:08 -0400

    populate students in profile index

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,7 +1,7 @@
 import { Profile } from "../models/profile.js";
 
 function index(req, res) {
-  Profile.find({})
+  Profile.find({}).populate('students')
     .then((profiles) => res.json(profiles))
     .catch((err) => {
       console.log(err);
